9733363dc7656c2fdb1ee15a31e54391bcba8c86
Rajesh Kumar <84396121+d3vrajesh@users.noreply.github.com>
2021-05-26 20:49:52 +0530

    Created the registration form in child theme
    Created the registration form in the child theme which collects input from the public WordPress page and stores the data on the database.

---
+++ functions.php
@@ -0,0 +1,243 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+
+//------Custom Style Sheet/CSS attachment (style.css)--------------
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		
+	);
+}
+
+//------Page title disable from theme --------------
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+function ele_disable_page_title( $return ) {
+return false;
+}
+add_filter( 'hello_elementor_page_title', 'ele_disable_page_title' );
+
+//------Custom Script attachment (mform.js) --------------
+add_action( 'wp_enqueue_scripts', 'my_custom_script_load' );
+function my_custom_script_load(){
+  wp_enqueue_script( 'my-custom-script', get_stylesheet_directory_uri() . '/mform', array( 'jquery' ) );
+}
+
+//==================Membership form ============== 
+if(isset($_POST['formsubmit']))
+{
+
+ //------Databse Access --------------
+	global $wpdb;
+
+ //------Variable mapping with html input fields using name --------------
+		$mname = $_POST["mname"]; 
+		$mdob = $_POST["mdob"];
+		$maddress = $_POST["maddress"];
+		$mtelres = $_POST["mtelres"];
+		$mteloff = $_POST["mteloff"];
+		$mmob = $_POST["mmob"];
+		$memail = $_POST["memail"];
+		$mprofession = $_POST["mprofession"];
+		$mnameins = $_POST["mnameins"];
+		$mplaceins = $_POST["mplaceins"];
+		$mdesignation = $_POST["mdesignation"];
+		$minterest = $_POST["minterest"];
+		$mamount = $_POST["mamount"];
+		$mrefname = $_POST["mrefname"];
+		$mrefdet = $_POST["mrefdet"];
+		$mplace = $_POST["mplace"];
+		$mdate =  date('Y-m-d');
+ 
+	//-----------Form input Validation-----------	
+	$error = array();
+
+	//-----Name - input text validation 
+	$fname = "/^[a-zA-Z\s]+$/";
+	if (!preg_match($fname, $mname)) {
+		$error['fname'] = "Invalid input";
+		$errorfname = "Invalid Input.";
+	}
+	//-----Address - input text validation 
+
+	$faddress = "/^[A-Za-z0-9\s\-\,]+$/";
+	if (!preg_match($faddress, $maddress)) {
+		$error['faddress'] = "Only numeric, alphabetic, dot, comma, hypen are allowed.";
+		$errorfaddress = "Only numeric, alphabetic, dot, comma, hypen are allowed.";
+	}
+
+	//-----Mobile number - input number validation 
+	$fmob = "/^[0-9]+$/";
+	$fmobno = strlen($_POST["mmob"]);
+
+	if (!preg_match($fmob, $mmob))  {
+		$error['fmob'] = "Only numeric values are allowed";
+		$errorfmobno = "Only numeric values are allowed";
+	}
+	if ($fmobno != 10) {
+		$error['fmob'] = "Mobile number is not valid";
+		$errorfmobno = "Mobile number is not valid";
+	}
+
+	//-----Telephone residential - input number validation 
+	if (!preg_match($fmob, $mtelres)) {
+		$error['ftelres'] = "Only numeric value is allowed.";
+		$errorftelres = "Only numeric value is allowed.";
+	}
+	
+	//-----Telephone office - input number validation 
+	if (!preg_match($fmob, $mteloff)) {
+		$error['fteloffice'] = "Only numeric value is allowed.";
+		$errorfteloffice = "Only numeric value is allowed.";
+	}
+
+	//-----Email - input email validation 
+	if (is_email($memail) == false) {
+		$error['mail'] = "Invalid e-mail.";
+		$errormail = "Invalid e-mail.";
+	}
+
+	//-----Email - input text validation
+	$fprofession = "/^[a-zA-Z\s]+$/";
+	if (!preg_match($fprofession, $mprofession)) {
+		$error['fprofession'] = "Invalid input.";
+		$errorfprofession = "Invalid input.";
+	}
+
+	//-----Name of institution - input text validation
+	$fnameins = "/^[a-zA-Z\s]+$/";
+	if (!preg_match($fnameins, $mnameins)) {
+		$error['fnameins'] = "Invalid input";
+		$errorfnameins = "Invalid input";
+	}
+
+	//-----Place of institution - input text validation
+	$fplaceins = "/^[a-zA-Z0-9\s\-\,\.]+$/";
+	if (!preg_match($fplaceins, $mplaceins)) {
+		$error['fplaceins'] = "Invalid input";
+		$errorfplaceins = "Invalid input";
+	}
+	//-----Designation - input text validation
+	$fdesignation = "/^[a-zA-Z\s]+$/";
+	if (!preg_match($fdesignation, $mdesignation)) {
+		$error['fdesignation'] = "Invalid input.";
+		$errorfdesignation = "Invalid input.";
+	}
+
+	//-----nature of interest - input text validation
+	$finterest = "/^[a-zA-Z\s\,\.]+$/";
+	if (!preg_match($finterest, $minterest)) {
+		$error['finterest'] = "Invalid input.";
+		$errorfinterest = "Invalid input.";
+	}
+
+	//-----Reference member name  - input text validation
+	$frefname = "/^[a-zA-Z\s]+$/";
+	if (!preg_match($frefname, $mrefname)) {
+		$error['frefnamet'] = "Invalid input.";
+		$errorfrefnamet = "Invalid input.";
+	}
+
+	//-----Reference member detail - input email/mobile number validation
+	 
+	//-----Reference Email validation  - input length validation
+	$frefdet = "/^[0-9]+$/";
+	$frefmobno = strlen($_POST["mrefdet"]);
+	 
+	if ((is_email($mrefdet) == false) && (!preg_match($frefdet, $mrefdet)))
+	{
+		$error['frefdet'] = "Not a valid input";
+		$errorfrefdet = "Not a valid input.";		
+	}
+	
+	if ((!preg_match($frefdet, $mrefdet) == false) && ($frefmobno != 10))
+	{
+		$error['frefdetmob'] = "Not a valid input";
+		$errorfrefdet = "Not a valid input.";		
+	 
+	} 
+	 
+
+	//-----Place of application - input text validation
+	$fplace = "/^[a-zA-Z]+$/";
+	if (!preg_match($fplace, $mplace)) {
+		$error['fplace'] = "Not a valid input.";
+		$errorfplace = "not a valid input.";
+	} 
+
+	//-----Upload file size validation (500Kb - Max)
+	if ($_FILES["mupload"]["size"] > 500000) {
+	$error['mupload'] = "File size is too  large. Maximum allowed file size is 500Kb";
+	$errorfupload = "File size is too  large. Maximum allowed file size is 500Kb";
+	} 
+
+	//-----Upload file format/extension validation (Allowed - jpeg, jpg, png) 
+
+	$file_jpeg = "jpeg";
+	$file_jpg = "jpg";
+	$file_png = "png";
+	$upload_file = wp_check_filetype($_FILES["mupload"]["name"]);
+	$upload_file_ext = $upload_file['ext'];
+
+	if (($upload_file_ext != $file_jpeg) && ($upload_file_ext != $file_jpg) && ($upload_file_ext != $file_png)) {
+		$error['mupload'] = "upload a png/jpeg/jpg file";
+		$errorfupload = "upload a png/jpeg/jpg file";
+		}
+		
+	//-----Get file upload URL   
+	$upload_dir = wp_upload_dir();
+	$mupload_url = $upload_dir['url']; 
+	$mupload = $mupload_url . "/" . $_FILES['mupload']['name'];
+
+	//-----Save form data in Database  
+	if (count($error) == 0) {
+		$data_array = array(
+			'name' => $mname,
+			'dob' => $mdob,
+			'address' => $maddress,
+			'tel_res' => $mtelres,
+			'tel_off' => $mteloff,
+			'mob' => $mmob,
+			'email' => $memail,
+			'profession' => $mprofession,
+			'name_ins' => $mnameins,
+			'place_ins' => $mplaceins,
+			'interest' => $minterest,
+			'designation' => $mdesignation,
+			'amount' => $mamount,
+			'ref_name' => $mrefname,
+			'ref_detail' => $mrefdet,
+			'place' => $mplace,
+			'date' => $mdate,
+			'upload' => $mupload
+			);
+
+				$table_name = 'wp_nnhs_membership';
+		$row_result = $wpdb->insert($table_name, $data_array, $format=null);
+		//wp_mail('rajeshr@keystone-foundation.org','Testing Form Submission',$message);
+		wp_upload_bits($_FILES['mupload']['name'], null, file_get_contents($_FILES['mupload']['tmp_name'])); 
+		$success = "Success";	
+		
+	}
+	else {
+		
+		
+		$submission_failed = 'You application is not submitted, Check all the inputs.';
+		
+
+	}
+}
+
---
+++ page-membership.php
@@ -0,0 +1,142 @@
+<?php
+/*
+ * Template Name: Membership
+ */
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+ ?>
+<!---START------------Page Template ------------->
+<?php get_header(); 
+
+require_once("functions.php");
+?>
+
+<div class="page-container">
+
+    <div class="mform-title">Apply Online for Membership</div>
+    <!---------------PHP code for HTML form ------------->
+    <?php 
+ 
+ ?>
+    <div class="container">
+
+        <!---form action  <?php /*echo htmlspecialchars($_SERVER["PHP_SELF"]); */?>-->
+
+        <form id="membership" method="post" action="" autocomplete="on" enctype="multipart/form-data">
+
+            <?php 
+                if ($row_result == 1) {
+                
+                    wp_mail('rajeshr@keystone-foundation.org','Testing Form Submission',$message);
+
+                // wp_redirect('http://localhost/nnhs/success/');
+                echo $success;              
+                }       
+                
+                else {
+                    echo $submission_failed;
+                    echo '<h5> <center>Fill all the required fields</center></h5>';
+                    
+                    }
+            ?>
+
+
+            <p><span class="membership-error">* required field</span></p>
+            <label for="mname"><b>Name</b></label> <span class="membership-error">* <?php echo $errorfname ;?></span>
+            <input type="text" placeholder="Name" name="mname" maxlength="20"
+                value="<?= isset($_POST['mname']) ? $_POST['mname'] : ''; ?>" required>
+
+            <label for="mdob"><b>Date of Birth</b></label> <span class="membership-error">*
+                <?php echo $errorfdob ;?></span>
+            <input type="date" placeholder="Date of Birth" name="mdob"
+                value="<?= isset($_POST['mdob']) ? $_POST['mdob'] : ''; ?>" required>
+
+            <label for="maddress"><b>Address</b></label> <span class="membership-error">*
+                <?php echo $errorfaddress ;?></span>
+            <textarea name="maddress" rows="5" cols="33" class="textarea" maxlength="200"
+                value="<?= isset($_POST['maddress']) ? $_POST['maddress'] : ''; ?>" required> Address</textarea>
+
+            <label for="mtelres"><b>Telephne-Residential</b></label><span class="membership-error">
+                <?php echo $errorftelres ;?></span>
+            <input type="tel" placeholder="Telephone Residential" name="mtelres"
+                value="<?= isset($_POST['mtelres']) ? $_POST['mtelres'] : ''; ?>">
+
+            <label for="mteloff"><b>Telephne-Office</b></label><span class="membership-error">
+                <?php echo $errorfteloffice ;?></span>
+            <input type="tel" placeholder="Telephone Office" name="mteloff"
+                value="<?= isset($_POST['mteloff']) ? $_POST['mteloff'] : ''; ?>">
+
+            <label for="mmob"><b>Mobile</b></label><span class="membership-error">* <?php echo $errorfmobno ;?></span>
+            <input type="tel" placeholder="Mobile" name="mmob"
+                value="<?= isset($_POST['mmob']) ? $_POST['mmob'] : ''; ?>" required>
+
+            <label for="memail"><b>Email</b></label><span class="membership-error">* <?php echo $errorfemail ;?></span>
+            <input type="text" placeholder="Enter Email" name="memail"
+                value="<?= isset($_POST['memail']) ? $_POST['memail'] : ''; ?>" required>
+
+            <label for="mprofession"><b>Profession</b></label><span class="membership-error">*
+                <?php echo $errorfprofession ;?></span>
+            <input type="text" placeholder="Profession" name="mprofession"
+                value="<?= isset($_POST['mprofession']) ? $_POST['mprofession'] : ''; ?>" required>
+
+            <label for="mnameins"><b>Name of Institution</b></label> <span class="membership-error">
+                <?php echo $errorfnameins ;?></span>
+            <input type="text" placeholder="Name of Institution" name="mnameins"
+                value="<?= isset($_POST['mnameins']) ? $_POST['mnameins'] : ''; ?>">
+
+            <label for="mplaceins"><b>Place of Institution</b></label> <span class="membership-error">
+                <?php echo $errorfplaceins ;?></span>
+            <input type="text" placeholder="Place of Institution" name="mplaceins"
+                value="<?= isset($_POST['mplaceins']) ? $_POST['mplaceins'] : ''; ?>">
+
+            <label for="mdesignation"><b>Designation</b></label> <span class="membership-error">
+                <?php echo $errorfdesignation ;?></span>
+            <input type="text" placeholder="Designation" name="mdesignation"
+                value="<?= isset($_POST['mdesignation']) ? $_POST['mdesignation'] : ''; ?>">
+
+            <label for="minterest"><b>Nature of Interest</b></label><span
+                class="membership-error">*<?php echo $errorfinterest ;?> </span>
+            <textarea name="minterest" rows="5" cols="33" class="textarea"
+                value="<?= isset($_POST['minterest']) ? $_POST['minterest'] : ''; ?>"
+                required> Describe your Nature of Interest..</textarea>
+
+            <label for="mamount"><b>Amount Rs.</b></label><span class="membership-error">*
+                <?php echo $errorfamount ;?></span>
+            <select name="mamount" value="<?= isset($_POST['mamount']) ? $_POST['mamount'] : ''; ?>" required>
+                <option value="1000">1,000/-</option>
+            </select>
+
+            <label for="mrefname"><b>Referred by NNHS member (Please mention Name) </b></label><span
+                class="membership-error">* <?php echo $errorfrefnamet ;?></span>
+            <input type="text" placeholder="Referral member's name" name="mrefname"
+                value="<?= isset($_POST['mrefname']) ? $_POST['mrefname'] : ''; ?>" required>
+
+            <label for="mrefdet"><b>Referral member's Phone/E-mail </b></label><span class="membership-error">*
+                <?php echo $errorfrefdet ;?></span>
+            <input type="text" placeholder="Referral member's detail" name="mrefdet"
+                value="<?= isset($_POST['mrefdet']) ? $_POST['mrefdet'] : ''; ?>" required>
+
+            <label for="place"><b>Place </b></label><span class="membership-error">* <?php $errorfplace ;?></span>
+            <input type="text" placeholder="place" name="mplace"
+                value="<?= isset($_POST['mplace']) ? $_POST['mplace'] : ''; ?>" required>
+
+            <label for="mupload"><b>Photo upload (Passport size, less than 500kb) </b></label><span
+                class="membership-error">* <?php echo $errorfupload ;?></span>
+            <input type="file" accept="image/png, image/jpeg, image/jpg" name="mupload" id="mupload"
+                value="<?= isset($_POST['mupload']) ? $_POST['mupload'] : ''; ?>" required>
+            <br><br><br>
+
+
+            <div class="mform-btn-container">
+                <div class="mform-btn">
+
+                    <button type="submit" class="registerbtn" name="formsubmit">Register</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!---------------HTML PAGE/Form DESIGN -END------------>
+<?php get_footer(); ?>
